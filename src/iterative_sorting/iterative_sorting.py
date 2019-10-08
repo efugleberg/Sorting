950150aa96d525b651a7320a76a43b18fc801cdc
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-08 14:43:19 -0500

    day 2 updates

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,17 +18,31 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    # for loop iterating at position[i] over the length of the array
     for i in range(len(arr)):
+        print('arrI', i)
+
+        # nested for loop to iterate through each item in the array at position[i]
+        # reduce the freq. of iterations by making length less than len(arr)
+        # as you move through the items at index[j], the bubble sorting happens naturally
+        # if an item at arr[j] is > than arr[j+1], it moves ahead, then the iteration starts
+        # over until it it's through that cycle of len(arr)-i-1.
         for j in range(len(arr)-i-1):
+            # print('j', j)
+            print('length', len(arr)-i-1)
+            # print('i', i)
+            print('arrJ', arr)
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
 
+test = [3, 5, 0, 1, 2, 4]
+bubble_sort(test)
+
+
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
-    
-   
 
     return arr
